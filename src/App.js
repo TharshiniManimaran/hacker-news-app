ce8e6e6beac4552489de442451b2071f6b1e1fe1
TharshiniManimaran <76949332+TharshiniManimaran@users.noreply.github.com>
2022-10-24 01:54:33 +0530

    my hackernews clone app initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route } from "react-router-dom";
+import { Switch } from "react-router-dom";
+
+import Page from "./pages/common/Page";
+import MainNavigation from "./components/layout/MainNavigation";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main">
+      <MainNavigation />
+      <Switch>
+        <Route path="/" exact>
+          <Page action="top" />
+        </Route>
+        <Route path="/top">
+          <Page action="top" />
+        </Route>
+        <Route path="/best">
+          <Page action="best" />
+        </Route>
+        <Route path="/new">
+          <Page action="new" />
+        </Route>
+        <Route path="/ask">
+          <Page action="ask" />
+        </Route>
+        <Route path="/job">
+          <Page action="job" />
+        </Route>
+        <Route path="/show">
+          <Page action="show" />
+        </Route>
+      </Switch>
     </div>
   );
 }
---
+++ src/components/Link.js
@@ -0,0 +1,5 @@
+function Link(props) {
+  return <a href={props.url}>{props.title}</a>;
+}
+
+export default Link;
